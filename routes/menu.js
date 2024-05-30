dee44b0e4de58b6f18cf875d1a7e6726a9307646
Nam Tran <namtran211101@gmail.com>
2024-05-30 17:30:51 +1000

    OOP menu.js controller

--- routes/menu.js
+++ routes/menu.js
@@ -1,30 +1,37 @@
 const express = require('express');
-const router = express.Router();
-const menuItem = require('../models/menuItem');
+const MenuItem = require('../models/menuItem');
 
-// Create -----------------------------------------------
-router.post('/', async (req, res) => {
-  try {
-    console.log(req.body);
-    const item = new menuItem(req.body);
-    console.log(item);
-    await item.save();
-    res.status(201).json(item);
-  } 
-  catch (error) {
-    res.status(400).json({ message: error.message });
+class MenuController {
+  constructor() {
+    this.router = express.Router();
+    this.initializeRoutes();
   }
-});
 
-// Read ------------------------------------------------
-// get all menu items
-router.get('/', async (req, res) => {
-  try {
-    const items = await menuItem.find();
-    res.send(items);
-  } 
-  catch (error) {
-    res.status(500).json({ message: error.message })
-  }});
+  initializeRoutes() {
+    this.router.post('/', this.createMenuItem.bind(this));
+    this.router.get('/', this.getAllMenuItems.bind(this));
+  }
 
-module.exports = router;
+  async createMenuItem(req, res) {
+    try {
+      console.log(req.body);
+      const item = new MenuItem(req.body);
+      console.log(item);
+      await item.save();
+      res.status(201).json(item);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+
+  async getAllMenuItems(req, res) {
+    try {
+      const items = await MenuItem.find();
+      res.send(items);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+}
+
+module.exports = new MenuController().router;
